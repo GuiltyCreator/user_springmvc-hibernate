88d4c619f077c9194eafccb959c5db3dd3a01ed9
GuiltuCeator <a727901974@126.com>
2019-07-27 16:19:15 +0800

    引入Spring MVC实现控制层

---
+++ src/main/Java/online/shixun/project/controller/UserController.java
@@ -0,0 +1,14 @@
+package online.shixun.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+@RequestMapping(value = "hello")
+    public String hello(){
+    return "hello";
+}
+
+}
