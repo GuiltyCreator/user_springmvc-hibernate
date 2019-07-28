d47c46f6b266e7d6d895a2c6bffe6dc7203a42fe
CupricNtrate <a727901974@126.com>
2019-07-28 08:16:22 +0800

    引入spring实现IoC特征

--- src/main/Java/online/shixun/project/controller/UserController.java
+++ src/main/Java/online/shixun/project/controller/UserController.java
@@ -1,14 +1,19 @@
 package online.shixun.project.controller;
 
+import online.shixun.project.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
 @RequestMapping(value = "hello")
     public String hello(){
-    return "hello";
+    return userService.test();
 }
 
 }
